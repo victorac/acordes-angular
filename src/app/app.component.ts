f53979439c29e0d113d653b8cfe9bf1b3180058f
victorac <victorde.ac@gmail.com>
2024-09-06 23:43:54 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,38 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'acordes';
+  @ViewChild('scrollable') scrollable: ElementRef | undefined;
+  ngAfterViewInit() {
+    const scrollable = this.scrollable?.nativeElement;
+    let isDown = false;
+    let startX: number;
+    let scrollLeft: number;
+
+    scrollable.addEventListener('mousedown', (e: { pageX: number; }) => {
+      isDown = true;
+      scrollable.classList.add('active');
+      startX = e.pageX - scrollable.offsetLeft;
+      scrollLeft = scrollable.scrollLeft;
+    });
+
+    scrollable.addEventListener('mouseleave', () => {
+      isDown = false;
+      scrollable.classList.remove('active');
+    });
+
+    scrollable.addEventListener('mouseup', () => {
+      isDown = false;
+      scrollable.classList.remove('active');
+    });
+
+    scrollable.addEventListener('mousemove', (e: { preventDefault: () => void; pageX: number; }) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - scrollable.offsetLeft;
+      const walk = (x - startX) * 3; //scroll-fast
+      scrollable.scrollLeft = scrollLeft - walk;
+    });
+  }
 }
