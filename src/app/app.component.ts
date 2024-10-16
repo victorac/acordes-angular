0a6ccddd5d5a7d3b5f579f9f6a04d9afcbd0b32d
victorac <victorde.ac@gmail.com>
2024-10-15 23:04:28 -0300

    add key-selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,27 +9,23 @@ import { StringComponent } from './string/string.component';
 import { ChipComponent } from './chip/chip.component';
 import { CaseComponent } from './case/case.component';
 import { NotesService } from './notes.service';
-import { NoteName, NOTES } from '../util/notes';
+import { NoteName } from '../util/notes';
+import { KeySelectorComponent } from './key-selector/key-selector.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StringComponent, ChipComponent, CaseComponent],
+  imports: [RouterOutlet, StringComponent, ChipComponent, CaseComponent, KeySelectorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'acordes';
   showKeySelect = false;
-  keySelectInitialized = false;
-  @ViewChild('scrollable') scrollable: ElementRef | undefined;
+  
+  
   strings: NoteName[] = [NoteName.E, NoteName.A, NoteName.D, NoteName.G, NoteName.B, NoteName.E];
-  allKeys: NoteName[] = NOTES;
 
-  private mouseDownListener: any;
-  private mouseLeaveListener: any;
-  private mouseUpListener: any;
-  private mouseMoveListener: any;
 
   constructor(private cdr: ChangeDetectorRef, private notesService: NotesService) {}
 
@@ -37,66 +33,7 @@ export class AppComponent {
     return this.notesService.getRoot();
   }
 
-  ngAfterViewChecked() {
-    if (this.showKeySelect && this.scrollable && !this.keySelectInitialized) {
-      this.initializeKeySelect();
-      this.keySelectInitialized = true;
-    } else if (!this.showKeySelect && this.keySelectInitialized) {
-      this.detachListeners();
-      this.keySelectInitialized = false;
-    }
-  }
-  initializeKeySelect() {
-    const scrollable = this.scrollable?.nativeElement;
-    let isDown = false;
-    let startX: number;
-    let scrollLeft: number;
 
-    this.mouseDownListener = (e: { pageX: number }) => {
-      isDown = true;
-      scrollable.classList.add('active');
-      startX = e.pageX - scrollable.offsetLeft;
-      scrollLeft = scrollable.scrollLeft;
-    };
-
-    this.mouseLeaveListener = () => {
-      isDown = false;
-      scrollable.classList.remove('active');
-    };
-
-    this.mouseUpListener = () => {
-      isDown = false;
-      scrollable.classList.remove('active');
-    };
-
-    this.mouseMoveListener = (e: {
-      preventDefault: () => void;
-      pageX: number;
-    }) => {
-      if (!isDown) return;
-      e.preventDefault();
-      const x = e.pageX - scrollable.offsetLeft;
-      const walk = (x - startX) * 1.2; //scroll-fast
-      scrollable.scrollLeft = scrollLeft - walk;
-    };
-
-    scrollable.addEventListener('mousedown', this.mouseDownListener);
-    scrollable.addEventListener('mouseleave', this.mouseLeaveListener);
-    scrollable.addEventListener('mouseup', this.mouseUpListener);
-    scrollable.addEventListener('mousemove', this.mouseMoveListener);
-  }
-  detachListeners() {
-    const scrollable = this.scrollable?.nativeElement;
-    if (scrollable) {
-      scrollable.removeEventListener('mousedown', this.mouseDownListener);
-      scrollable.removeEventListener('mouseleave', this.mouseLeaveListener);
-      scrollable.removeEventListener('mouseup', this.mouseUpListener);
-      scrollable.removeEventListener('mousemove', this.mouseMoveListener);
-    }
-  }
-  selectKey(key: NoteName) {
-    this.notesService.setRoot(key);
-  }
   exitKeySelect() {
     this.showKeySelect = false;
   }
@@ -106,7 +43,5 @@ export class AppComponent {
       this.cdr.detectChanges();
     }
   }
-  ngOnDestroy() {
-    this.detachListeners();
-  }
+  
 }
