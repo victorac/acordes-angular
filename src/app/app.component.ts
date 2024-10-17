f36eb4551285f3ed7d0f4b7ab6002cc18c38e8fb
victorac <victorde.ac@gmail.com>
2024-10-16 21:25:20 -0300

    add toggle minor 3 and major 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
-import {
-  ChangeDetectorRef,
-  Component,
-} from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StringComponent } from './string/string.component';
 import { ChipComponent } from './chip/chip.component';
 import { CaseComponent } from './case/case.component';
-import { NotesService } from './notes.service';
 import { KeySelectorComponent } from './key-selector/key-selector.component';
 import { ArmComponent } from './arm/arm.component';
 import { CommonModule } from '@angular/common';
+import { NotesConfigService } from './notes-config.service';
 
 @Component({
   selector: 'app-root',
@@ -32,7 +29,7 @@ export class AppComponent {
 
   constructor(
     private cdr: ChangeDetectorRef,
-    private notesService: NotesService
+    private notesConfigService: NotesConfigService
   ) {}
 
   exitKeySelect() {
@@ -43,5 +40,21 @@ export class AppComponent {
     if (this.showKeySelect) {
       this.cdr.detectChanges();
     }
+  }
+
+  toggleMinor() {
+    this.notesConfigService.toggleMinor();
+  }
+
+  toggleMajor7() {
+    this.notesConfigService.toggleMajor7();
+  }
+
+  showMinor() {
+    return this.notesConfigService.showMinor;
+  }
+
+  showMajor7() {
+    return this.notesConfigService.showMajor7;
   }
 }
--- src/app/notes-config.service.ts
+++ src/app/notes-config.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Interval, NoteName } from '../util/notes';
+import { Interval } from '../util/notes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotesConfigService {
   private includedIntervals: Interval[] = [
@@ -11,10 +11,13 @@ export class NotesConfigService {
     Interval.MajorThird,
     Interval.PerfectFifth,
     Interval.MinorSeventh,
-    Interval.MajorSeventh
+    Interval.MajorSeventh,
   ];
 
-  constructor() { }
+  showMajor7: boolean = true;
+  showMinor: boolean = true;
+
+  constructor() {}
 
   getIncludedIntervals(): Interval[] {
     return this.includedIntervals;
@@ -23,4 +26,27 @@ export class NotesConfigService {
   isIntervalIncluded(interval: Interval): boolean {
     return this.includedIntervals.includes(interval);
   }
+
+  toggleMinor() {
+    this.showMinor = !this.showMinor;
+    if (this.showMinor) {
+      this.includedIntervals.push(Interval.MinorThird);
+    } else {
+      this.includedIntervals = this.includedIntervals.filter(
+        (interval) => interval !== Interval.MinorThird
+      );
+    }
+  }
+
+  toggleMajor7() {
+    this.showMajor7 = !this.showMajor7;
+    if (this.showMajor7) {
+      this.includedIntervals.push(Interval.MajorSeventh);
+    } else {
+      this.includedIntervals = this.includedIntervals.filter(
+        (interval) => interval !== Interval.MajorSeventh
+      );
+    }
+  }
+
 }
